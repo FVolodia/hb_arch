2a90d1e0676299d278b9ba5d99aeed6ba9e958f4
kkovalenko <kk@empat.tech>
2023-08-03 17:20:06 +0300

    Update color scheme

--- app/src/main/java/com/homebody/ui/theme/Theme.kt
+++ app/src/main/java/com/homebody/ui/theme/Theme.kt
@@ -3,40 +3,11 @@ package com.homebody.ui.theme
 import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
-
-private val DarkColorScheme = darkColorScheme(
-    primary = PrimaryColor,
-    onPrimary = OnPrimary,
-    primaryContainer = PrimaryContainerDark,
-    onPrimaryContainer = OnPrimaryContainerDark,
-    secondary = SecondaryColor,
-    onSecondary = OnSecondaryColor,
-    background = BackgroundDark,
-    onBackground = OnBackgroundDark,
-    surface = SurfaceDark,
-    onError = onErrorDark,
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = PrimaryColor,
-    onPrimary = OnPrimary,
-    primaryContainer = PrimaryContainerLight,
-    onPrimaryContainer = OnPrimaryContainerLight,
-    secondary = SecondaryColor,
-    onSecondary = OnSecondaryColor,
-    background = BackgroundLight,
-    onBackground = OnBackgroundLight,
-    surface = SurfaceLight,
-    errorContainer = ErrorContainerLight,
-    onError = onErrorLight
-)
 
 @Composable
 fun AppTheme(
@@ -44,8 +15,8 @@ fun AppTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> HomebodyDarkColors
+        else -> HomebodyLightColors
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
